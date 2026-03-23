a782d57e3efd63a7548e0b721f1af86dab691e0b
Strike32GT <roberto.mas@tecsup.edu.pe>
2026-03-23 12:57:37 -0500

    Ejercicio extra hecho

--- Lab02_FernandoMas.playground/Contents.swift
+++ Lab02_FernandoMas.playground/Contents.swift
@@ -37,4 +37,62 @@ print("El pago total es: $\(PagoTotal)")
 
 
 
-//son 4 funciones, 
+//son 4 funciones
+import Foundation
+
+func funcion1(texto: String, numero: Int) {
+    if texto.uppercased().contains("TECSUP") {
+        if numero >= 1 {
+            print("Pasión por la tecnología, TEC mi casita")
+        } else {
+            print("Cantidad de recorrido no suficiente para llegar a TECSUP")
+        }
+        return
+    } else if texto.uppercased().contains("MIT") {
+        if numero >= 2 {
+            print("MIT es Tecsup premium")
+        } else {
+            print("Cantidad de recorrido no suficiente para llegar a MIT")
+        }
+        return
+    } else if texto.uppercased().contains("UNI") {
+        if numero >= 3 {
+            funcion2(texto: texto, numero: numero)
+        } else {
+            print("Cantidad de recorrido no suficiente para llegar a UNI")
+        }
+    } else if texto.uppercased().contains("UNALM") {
+        if numero >= 4 {
+            funcion3(texto: texto, numero: numero)
+        } else {
+            print("Cantidad de recorrido no suficiente para llegar a UNALM")
+        }
+    } else if texto.uppercased().contains("UPM") {
+        if numero >= 5 {
+            funcion4(texto: texto, numero: numero)
+        } else {
+            print("Cantidad de recorrido no suficiente para llegar a UPM")
+        }
+    } else {
+        print("No se encontró información de \(texto)")
+    }
+}
+
+func funcion2(texto: String, numero: Int) {
+    print("Esto es la UNI, los marcianos")
+}
+
+func funcion3(texto: String, numero: Int) {
+    print("Esto es la UNALM aa")
+}
+
+func funcion4(texto: String, numero: Int) {
+    print("Esto es la UPM")
+}
+
+// Ejemplos
+funcion1(texto: "Busca UNALM", numero: 1)   
+funcion1(texto: "Busca UNALM", numero: 4)
+funcion1(texto: "Busca UPM", numero: 3)
+funcion1(texto: "Busca UPM", numero: 5)
+
