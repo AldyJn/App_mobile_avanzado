a6ed529d7d443b84387031f0beeed5cbec1bbe22
AldyJn <aldy.montoya@tecsup.edu.pe>
2026-03-23 11:59:22 -0500

    Caso 3

--- laboratorio_2/MyPlayground.playground/Contents.swift
+++ laboratorio_2/MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+func calcularCalorias(actividad: String, minutos: Int) -> Double {
+    var caloriasPorMinuto: Double = 0.0
+    
+    if actividad == "dormir" {
+        caloriasPorMinuto = 1.08
+    } else if actividad == "sentado" {
+        caloriasPorMinuto = 1.66
+    } else {
+        print("Actividad no permitida: \(actividad)")
+        return 0
+    }
+    
+    return Double(minutos) * caloriasPorMinuto
+}
+
+
+func mostrarCalorias(actividad: String, minutos: Int) {
+    let calorias = calcularCalorias(actividad: actividad, minutos: minutos)
+    print("Calorías consumidas durante \(minutos) minutos de \(actividad): \(String(format: "%.2f", calorias)) kcal")
+}
+mostrarCalorias(actividad: "sentado", minutos: 123)
